65a98c7f5555011965859a158e8558a0d21d6561
attacker0258 <77044381+attacker0258@users.noreply.github.com>
2023-10-06 23:58:17 +0530

    Update rss_bot.py

--- .github/scripts/rss_bot.py
+++ .github/scripts/rss_bot.py
@@ -2,6 +2,7 @@ import feedparser
 import os
 
 def fetch_and_create_files(rss_url):
+    print("Fetching RSS feed...")
     feed = feedparser.parse(rss_url)
 
     folder_path = "articles"
@@ -15,6 +16,7 @@ def fetch_and_create_files(rss_url):
         file_name = f"{folder_path}/{title.lower().replace(' ', '_')}.md"
         with open(file_name, 'w') as file:
             file.write(file_content)
+        print(f"File created: {file_name}")
 
 if __name__ == "__main__":
     rss_url = 'https://blog.intigriti.com/category/bugbytes/feed/'
